201e998211df16b1a934f811db033a29c4565a25
organichottie <mmatlitech@gmail.com>
2025-05-04 02:46:53 +0200

    JS added
    Added some JS for form submission and to display in typewriter form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-//alert("hellowwwww!");
+function generatePoem(event) {
+  event.preventDefault();
+  //alert("Generating Poem...");
+
+  new Typewriter("#poem", {
+    strings: "Thia poem is really dope",
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
